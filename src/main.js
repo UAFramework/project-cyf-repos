362a5d5479aeef9d954f1b80be8d408d9ee578cc
Pavel <pavel.filkovskiy@gmail.com>
2023-07-29 11:01:40 +0100

    complete level 1

--- src/main.js
+++ src/main.js
@@ -13,7 +13,8 @@ const options = {
 
 function buildPage() {
     const root = document.querySelector("div#root");
-    root.innerText = "Hello!";
+    const table = createTable(repos);
+    root.appendChild(table);
 }
 
 function search(searchTerm) {
@@ -33,5 +34,48 @@ function setPagination(recordsOnPage) {
     // and use this function in event handler 
     // for Pagination Component
 }
+
+function createTable(data) {
+    const table = document.createElement("table");
+    const thead = document.createElement("thead");
+    const tbody = document.createElement("tbody");
+   
+    table.append(thead, tbody);
+    
+    const headers = ["Name", "Description", "Link", "Updated At"]
+    .map(header => {
+        const th = document.createElement("th");
+        th.innerText = header;
+        return th;
+    });
+
+    thead.append(...headers);
+
+    const trows = data
+        .map( ({name, updated_at, description, html_url}) => {
+            const tr = document.createElement("tr");
+            
+            const tdName = document.createElement("td")
+            tdName.innerText = name;
+
+            const tdDescription = document.createElement("td") 
+            tdDescription.innerText = description;
+
+            const tdUrl = document.createElement("td") 
+            tdUrl.innerText = html_url;
+                                    
+            const tdUpdatedAt = document.createElement("td")
+            tdUpdatedAt.innerText = updated_at;
+
+            tr.append(tdName, tdDescription, tdUrl, tdUpdatedAt);
+            
+            return tr;
+
+        });
+
+    tbody.append(...trows);
+    return table;
+}
+
 
 export {buildPage};
