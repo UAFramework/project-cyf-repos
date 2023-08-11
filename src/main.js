370231dba69c47b3571d706c9388a9d54bb18a3c
Pavel <pavel.filkovskiy@gmail.com>
2023-08-11 22:23:00 +0100

    implement pagination

--- src/main.js
+++ src/main.js
@@ -3,69 +3,61 @@ import { repos } from "./data/repos.js";
 // global `options` objects that stores options
 // chosen by the user of the web-page.
 const config = {
-    filterby: null,
+    filterby: "",
     sortBy: {
         field: "name",
         direction: "asc" // "asc" | "desc"
     },
-    recordsOnPage: 10
+    pagination: {
+        recordsOnPage: 10,
+        currentPage: 1
+    }
 };
 
 function buildPage() {
     const root = document.querySelector("div#root");
     
-    const table = createTable(repos);
     const searchBar = createSearchComponent();
-    const sortComponent = createSortComponent()
-
-    root.append(searchBar, sortComponent, table);
+    const table = createTableComponent();
+    const sortingAndPagination = buildSortingAndPaginationComponent();
+    const pageNav = createPageNavComponent();
+    root.append(searchBar, sortingAndPagination, table, pageNav);
+    
+    renderPage(repos);
 }
 
 function search(searchTerm) {
-    
-    if (searchTerm === "") {
-        alert("Please give me searching criteria!");
-        return;
-    }
-
     config.filterby = searchTerm;
-
-    // refresh/ubdate table
-    const trows = convertReposToTableRows(repos)
-    const tbody = document.querySelector("table > tbody");
-    tbody.replaceChildren(...trows);
-    
+    renderPage(repos);
 }
 
 function sort({field, direction}) {
-    // implement sort functionality
-    // and use this function in event handlers
-    // for input and button elements in Sorting Component.
-
     config.sortBy.field = field;
-    config.sortBy.direction = direction;
-    
-    const trows = convertReposToTableRows(repos);
-    const tbody = document.querySelector("table > tbody");
-    tbody.replaceChildren(...trows);
+    config.sortBy.direction = direction; 
+    renderPage(repos);
+
 }
 
 function setPagination(recordsOnPage) {
-    // implement pagination functionality
-    // and use this function in event handler 
-    // for Pagination Component
+    config.pagination.recordsOnPage = Number(recordsOnPage);
+    renderPage(repos);
 }
 
+function switchPage(newPageNumer) {
+    config.pagination.currentPage = newPageNumer;
+    renderPage(repos);
+}
 
-function convertReposToTableRows(repos) {
-
-    const filteredRepos = config.filterby == null ? [...repos] :
-        repos.filter(({name, description}) => 
+function renderPage(repos) {
+    // prepare data for displaying on table (apply config):
+    const filteredRepos = config.filterby === "" ? [...repos] : repos
+        .filter(({name, description}) => 
                 // condition1 || (condition2.1 && condition2.2)
-                name.includes(config.filterby || 
-                    (description != null && description.includes(config.filterby))
+                name.toLowerCase().includes(config.filterby.toLowerCase() || 
+                    (description != null && 
+                        description.toLowerCase().includes(config.filterby.toLowerCase()))
         ));
-
+    
     filteredRepos.sort((repo1, repo2) => {
         let a = null;
         let b = null;
@@ -91,8 +83,32 @@ function convertReposToTableRows(repos) {
         return 0;
 
     });
+
+    let numberOfPages = Math.ceil(filteredRepos.length / config.pagination.recordsOnPage);
+    let startIndex = (config.pagination.currentPage - 1) * config.pagination.recordsOnPage;
+    let endIndex = startIndex + config.pagination.recordsOnPage;
+
+    const reposToDisplay = filteredRepos.slice(startIndex, endIndex);
     
-    return filteredRepos
+    renderTable(reposToDisplay);
+    renderPageNav(numberOfPages);
+}
+
+function renderTable(data) {
+    // render Table Head: 
+    const thead = document.querySelector("table > thead");
+    const headers = ["Name", "Description", "Link", "Updated At"]
+        .map(header => {
+            const th = document.createElement("th");
+            th.innerText = header;
+            return th;
+        });
+
+    thead.replaceChildren(...headers);
+
+    // render Table Rows:
+    const tbody = document.querySelector("table > tbody");
+    const trows = data
         .map( ({name, updated_at, description, html_url}) => {
             const tr = document.createElement("tr");
             
@@ -105,7 +121,7 @@ function convertReposToTableRows(repos) {
             const tdUrl = document.createElement("td");
             const a = document.createElement("a");
             a.href = html_url;
-            a.innerText = html_url;
+            a.innerText = html_url.replace("https://github.com/", "");
             tdUrl.appendChild(a);
                                     
             const tdUpdatedAt = document.createElement("td");
@@ -115,28 +131,41 @@ function convertReposToTableRows(repos) {
             
             return tr;
         });
+
+    tbody.replaceChildren(...trows);
 }
 
-function createTable(data) {
+
+function renderPageNav(numberOfPages) {
+    const pageLinks = [];
+    for (let i = 1; i <= numberOfPages; i++) {
+            const a = document.createElement("a");
+            
+            a.href = "#";
+            a.id = `page-${i}`;
+            a.className = i == config.pagination.currentPage ? "current" : "";
+            a.innerText = i;
+            
+            a.onclick = () => switchPage(i);
+            pageLinks.push(a);
+    }
+    
+    const pageNav = document.querySelector("#page-nav");
+    pageNav.replaceChildren(...pageLinks);
+}
+
+function createTableComponent() {
+    const div = document.createElement("div");
+    div.id = "table-container";
+
     const table = document.createElement("table");
     const thead = document.createElement("thead");
     const tbody = document.createElement("tbody");
    
     table.append(thead, tbody);
+    div.appendChild(table);
     
-    const headers = ["Name", "Description", "Link", "Updated At"]
-        .map(header => {
-            const th = document.createElement("th");
-            th.innerText = header;
-            return th;
-        });
-
-    thead.append(...headers);
-    const trows = convertReposToTableRows(data);
-    
-
-    tbody.append(...trows);
-    return table;
+    return div;
 }
 
 function createSearchComponent() {
@@ -163,9 +192,24 @@ function createSearchComponent() {
     return div;
 }
 
-function createSortComponent() {
+function buildSortingAndPaginationComponent() {
     const div = document.createElement("div");
+    div.id = "sorting-and-pagination-container";
+
+    const sortingComponent = createSortingSelector();
+    const paginationComponents = createPaginationSelector();
+    
+    div.append(sortingComponent, paginationComponents);
+    return div;
+
+}
+
+function createSortingSelector() {
+    const label = document.createElement("label");
+    
     const select = document.createElement("select");
+    select.name = "sort-by";
+
     const button = document.createElement("button");
 
     const directions = [{value: "asc", html: "A-Z &darr;"}, {value: "desc", html: "Z-A &uarr;"}];
@@ -184,17 +228,17 @@ function createSortComponent() {
     button.value = config.sortBy.direction;
     button.innerHTML = config.sortBy.direction === "asc" ? "A-Z &darr;" : "Z-A &uarr;";
 
-    div.id = "sort-container";
-    div.append(select, button);
+    label.append("Sort by: ", select, button);
 
     select.addEventListener("change", event => {
         let field = event.target.value;
-        let direction = document.querySelector("#sort-container > button").value;
+        let direction = button.value;
         sort({field, direction});
     });
     
     button.addEventListener("click", event => {
-        let field = document.querySelector("#sort-container > select").value;
+        let field = select.value;
+        
         let direction = event.target.value === "asc" ? "desc" : "asc";
         
         const {value, html} = directions.find(({value}) => 
@@ -206,6 +250,41 @@ function createSortComponent() {
         sort({field, direction});
     });
 
+    return label;
+}
+
+function createPaginationSelector() {
+    const label = document.createElement("label");
+    const select = document.createElement("select");
+    select.name = "pagination";
+
+    const options = [
+            {value: "10", text: "10"}, 
+            {value: "20", text: "20"},
+            {value: "50", text: "50"},
+            {value: "100", text: "100"}
+        ]
+        .map(({value, text}) => {
+            const option = document.createElement("option");
+            option.value = value;
+            option.innerText = text;
+
+            return option;
+        });
+    
+    select.append(...options);
+    label.append("Records on the page: ", select);
+
+    select.addEventListener("change", event => {
+        setPagination(event.target.value);
+    });
+
+    return label;
+}
+
+function createPageNavComponent() {
+    const div = document.createElement("div");
+    div.id = "page-nav";
     return div;
 }
 
